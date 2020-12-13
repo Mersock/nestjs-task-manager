605e17b20c63060523575b6c324b6b97da104b50
Knight <knzphumthawan@gmail.com>
2020-12-13 11:25:25 +0700

    add validate pipe.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,13 +14,14 @@ import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/tasks-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filter: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filter: GetTasksFilterDto): Task[] {
     if (Object.keys(filter).length) {
       return this.tasksService.getTaskWithFilters(filter);
     } else {
@@ -42,7 +43,7 @@ export class TasksController {
   @Patch('/:id/status')
   updateTask(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
